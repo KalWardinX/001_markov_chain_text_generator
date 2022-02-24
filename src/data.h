dd95eddc3b92737fc0b0a6525c53224bae3bce12
kalwardinX <akashrawat44444@gmail.com>
2022-02-24 11:43:49 +0530

    created transition diag

--- src/data.h
+++ src/data.h
@@ -9,3 +9,23 @@ class table{
         std::string get_next(std::string);
         void insert(std::string , std::string );
 };
+
+class transition_diag{
+    private:
+        std::map<std::string, int> m;
+        struct node{
+            std::string word;
+            int count;
+            node(std::string w, int c){
+                word = w;
+                count = c;
+            }
+        };
+        std::vector< std::vector<node*> > graph;
+        int size;
+        int get_pos(std::string);
+        void insert_map(std::string);
+        void insert_graph(int , std::string );
+    public:
+        void insert(std::string, std::string);
+};
---
+++ src/graph.cpp
@@ -0,0 +1,26 @@
+#include "data.h"
+
+int transition_diag::get_pos(std::string s){
+return m.find(s)->second;
+}
+
+void transition_diag::insert_map(std::string key){
+    if( m.find(key) == m.end()){
+        m.insert({key,size});
+        size++;
+    }
+}
+void transition_diag::insert_graph(int pos , std::string value){
+    for(auto n : graph[pos]){
+        if(n->word.compare(value)){
+            n->count++;
+            return;
+        }
+    }
+    graph[pos].push_back(new node(value,1));
+}
+void transition_diag::insert(std::string key, std::string value){
+    this->insert_map(key);
+    int pos_in_graph = this->get_pos(key);
+    this->insert_graph(pos_in_graph , value);
+}
